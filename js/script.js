106cae5606b17b0a0f39f2e2f8cd04d3d0c93a8e
ekaterina-chernykh <ekaterina-chernykh@users.noreply.github.com>
2022-11-22 02:53:56 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -24,10 +24,10 @@ function myButtonClicked() {
   if (buttonPositiveChecked == true) {
     let randomNumber = Math.floor(Math.random() * 6) + 1
     document.getElementById("radio-button-value").innerHTML =
-    "Value is: " + randomNumber
+      "Value is: " + randomNumber
   } else {
     let randomNumber = Math.floor(Math.random() * -6) + 1
     document.getElementById("radio-button-value").innerHTML =
-    "Value is: " + randomNumber
+      "Value is: " + randomNumber
   }
 }
